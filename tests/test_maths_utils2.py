9c7321e2dc877f3897924636073b398ca778d0ef
Nermeen Latif <nermeen.latif@gmail.com>
2026-01-19 16:28:11 +0000

    Add maths_utils module + parametrised pytest tests

---
+++ tests/test_maths_utils2.py
@@ -0,0 +1,10 @@
+import pytest
+from src.maths_utils import add
+
+@pytest.mark.parametrize("a, b, expected", [
+    (2, 3, 5),
+    (-1, 1, 0),
+    (0, 0, 0)
+])
+def test_add(a, b, expected):
+    assert add(a, b) == expected
